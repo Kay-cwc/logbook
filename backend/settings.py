8ad08bb0f8913c940ef1794a94f8271ed1b85ce7
kayton chiu <kayton.chiu@letlink.hk>
2020-10-21 07:58:38 +0800

    added authentication method

--- backend/settings.py
+++ backend/settings.py
@@ -67,10 +67,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-    ],
 }
 
 ROOT_URLCONF = 'backend.urls'
