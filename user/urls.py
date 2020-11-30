8373cacbeea42bfb8917d42792ac795dce1e19b8
kayton chiu <kayton.chiu@letlink.hk>
2020-12-01 05:01:17 +0800

    regex bugfix -> use path

--- user/urls.py
+++ user/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_auth.urls')),
     path('auth/registration', include('rest_auth.registration.urls')),
-    path('auth/activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
+    path('auth/activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('account/', include('allauth.urls')),
     path('search/', SearchUserViewSet.as_view({'get': 'search'})),
 ]
